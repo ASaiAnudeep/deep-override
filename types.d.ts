393135641fb0891409ac2a53783c553a7ed749a9
HimChin <asa.anudeep4@gmail.com>
2020-09-03 20:08:45 +0530

    updated readme with example & types

--- types.d.ts
+++ types.d.ts
@@ -1,2 +1,4 @@
-export function override<T, U>(target:T, source:U): T & U;
-export function override<T, U, V>(target:T, source1:U, source2: V): T & U & V;
+declare function override<T, U>(target:T, source:U): T & U;
+declare function override<T, U, V>(target:T, source1:U, source2: V): T & U & V;
+declare namespace override {}
+export = override;
